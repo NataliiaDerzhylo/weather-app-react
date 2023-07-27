ee973808976935639a34e6688c57f86d591e8b94
NataliiaDerzhylo <natali.dergilo@gmail.com>
2023-07-27 16:17:33 +0300

    Added improvements

--- src/App.js
+++ src/App.js
@@ -1,69 +1,83 @@
-import logo from "./logo.png";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-import React from "react";
+export default function SearchEngine() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [message, setMessage] = useState("");
+  const [loaded, setLoaded] = useState(false);
 
-export default function () {
+  function displayResult(response) {
+    setLoaded(true);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2980ff43226d67e53abfcdb6d457dcc8&units=metric`;
+    axios.get(url).then(displayResult);
+  }
+
   return (
     <div className="container">
       <div className="weather-app-wrapper">
-        <div className="weather-app">
-          <form id="search-form" className="mb-">
-            <div className="row">
-              <div className="col-10">
-                <input
-                  type="search"
-                  placeholder="Type a city"
-                  className="form-control"
-                  id="city-input"
-                  autoComplete="off"
-                />
-              </div>
-              <div className="col-2">
-                <input
-                  type="submit"
-                  value="search"
-                  className="btn btn-primary w-100"
-                  id="search-button"
-                />
-              </div>
-            </div>
-          </form>
-          <div className="overview">
-            <div className="row">
-              <div className="col-8"></div>
-              <div className="col-4">
-                <button className="button-current">Current Location</button>
-              </div>
-            </div>
-            <ul>
-              <li id="date"></li>
-              <li id="description"></li>
-            </ul>
-          </div>
+        <form id="search-form" className="mb-" onSubmit={handleSubmit}>
           <div className="row">
-            <div className="col-6">
-              <div className="d-flex weather-temperature">
-                <div>
-                  <strong id="temperature"></strong>
-                  <span className="units">°C</span>
+            <div className="col-10">
+              <input
+                type="search"
+                placeholder="Type a city"
+                className="form-control"
+                id="city-input"
+                autoComplete="off"
+                value={city}
+                onChange={updateCity}
+              />
+            </div>
+            <div className="col-2">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-primary w-100"
+                id="search-button"
+              />
+            </div>
+          </div>
+        </form>
+        {loaded && weatherData && (
+          <div className="weather-details">
+            <div className="overview">
+              <div className="row">
+                <div className="col-8">
+                  <h1>{city}</h1>
+                  <p>{weatherData.description}</p>
+                  <p>Temperature: {weatherData.temperature} °C</p>
+                  <p>Humidity: {weatherData.humidity} %</p>
+                  <p>Wind: {weatherData.wind} km/h</p>
+                </div>
+                <div className="col-4">
+                  <img
+                    src={weatherData.icon}
+                    alt={weatherData.description}
+                    className="weather-icon"
+                  />
                 </div>
               </div>
             </div>
-
-            <div className="col-6">
-              <ul>
-                <li>
-                  Humidity: <span id="humidity"></span>%
-                </li>
-                <li>
-                  Wind:<span id="wind"></span> km/h
-                </li>
-              </ul>
-            </div>
           </div>
-          <div className="weather-forecast" id="forecast"></div>
-        </div>
+        )}
+        {message && <p>{message}</p>}
         <footer>
           <small>
             <span className="name">by Nataliia Derzhylo</span>
